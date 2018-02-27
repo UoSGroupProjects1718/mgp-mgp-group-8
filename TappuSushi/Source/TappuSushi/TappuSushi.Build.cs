50277d2fe4489d54197615edccc7f63bcee26866
Alex <s175585@ucs.ac.uk>
2018-02-27 12:55:26 +0000

    Fixed merge

--- TappuSushi/Source/TappuSushi/TappuSushi.Build.cs
+++ TappuSushi/Source/TappuSushi/TappuSushi.Build.cs
@@ -8,20 +8,11 @@ public class TappuSushi : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-<<<<<<< HEAD
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-=======
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
->>>>>>> f0e497458d69906a810a453fa83f62f6f319b315
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
