430612d153e155762e82698c3e37e7b85cfdd041
Michael Davis <MichaelRDavis1991@protonmail.com>
2018-03-08 17:55:10 +0000

    Commit WIP on creating extra food classes

---
+++ UnityProject/Group8/Assets/Scripts/Food.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public GameObject food;
+    public LevelManager level;
+
+    [Range(0.0f, 6.0f)]
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
+
+public class Sushi : Food
+{
+    public Sushi()
+    {
+        
+    }
+}
