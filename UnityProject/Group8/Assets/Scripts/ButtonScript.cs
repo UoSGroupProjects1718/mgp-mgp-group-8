6ec4a278ac251b21c068b0b315b52d51057cd89f
Michael Davis <MichaelRDavis1991@protonmail.com>
2018-03-16 21:01:32 +0000

    All UI buttons now play sound

--- UnityProject/Group8/Assets/Scripts/ButtonScript.cs
+++ UnityProject/Group8/Assets/Scripts/ButtonScript.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour {
 
     public Button replay;
     public Button menu;
 
+    public void Play()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
 	// Use this for initialization
 	void Start () {
 
-        gameObject.GetComponent<AudioSource>().Play();
+        
 	}
 	
 	// Update is called once per frame
